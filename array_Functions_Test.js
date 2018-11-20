0778f82359aa8fffb488d05a141c112ca9266cd0
Suman Rajput <sumanraj@in-sumanraj.local>
2018-11-20 18:50:32 +0530

    array test has modified

--- array_Functions_Test.js
+++ array_Functions_Test.js
@@ -25,41 +25,104 @@ const sum = function(accumulator,element){
   return accumulator+element;
 }
 
-const testSlice = function(){
-  let array = [1,2,3,4,5,6,7];
-  deepEqual(slice(array),array);
-  deepEqual(slice(array,0,5),[1,2,3,4,5]);
-  deepEqual(slice(array,3,5),[4,5,]);
-  deepEqual(slice(array,array.length),[]);
+const factorial = function(accumulator,element) {
+  return accumulator*element;
 }
 
-const testFill = function() {
-  deepEqual(fill([1],"*"),["*"]);
-  deepEqual(fill([],"*"),[]);
-  deepEqual(fill([1,2,3]," "),[" "," "," "]);
-  deepEqual(fill([1,2,3],1),[1,1,1]);
-}
+describe("array functions",function(){
 
-const testFilter = function(){
-  deepEqual(filter(isEven,[1,2,3,4,4,2,5,6]),[2,4,4,2,6]);
-  deepEqual(filter(isOdd,[3,4,5,6]),[3,5]);
+  let array ;
+  beforeEach(function(){
+    array = [1,2,3,4,5,6,7];
+  });
 
-}
+  describe("slice",function(){
 
-const testMap = function() {
-  deepEqual(map(getSquare,[1]),[1]);
-  deepEqual(map(getSquare,[1,2,3]),[1,4,9]);
-}
+    it("if index values are not given, it should return same array",function(){ 
+      deepEqual(slice(array),array);
+    });
 
-const testReduce = function() {
-  deepEqual(reduce(sum,[1,2,3,4,5,6]),21);
-  deepEqual(reduce(sum,[]),undefined);
-  deepEqual(reduce(sum,[],2),2);
-  deepEqual(reduce(sum,[1,-1],-2),-2);
-}
+    it("it should give slice from 0th index to 5th index",function(){
+      deepEqual(slice(array,0,5),[1,2,3,4,5]);
+    });
 
-testSlice();
-testFill();
-testFilter();
-testMap();
-testReduce();
+    it("it should give slice from 3th to 5th index",function(){
+      deepEqual(slice(array,3,5),[4,5,]);
+    });
+
+    it("it should give empty array",function(){
+      deepEqual(slice(array,array.length),[]);
+    });
+  });
+
+  describe("fill",function(){
+
+    it("it should give one element in an array",function(){
+      deepEqual(fill([1],"*"),["*"]);
+    });
+
+    it("it should give an empty array",function(){
+      deepEqual(fill([],"*"),[]);
+    });
+
+    it("it should give an array of three space element",function(){
+      deepEqual(fill([1,2,3]," "),[" "," "," "]);
+    });
+
+    it("it should give an array of three element",function(){
+      deepEqual(fill([1,2,3],1),[1,1,1]);
+    });
+  });
+
+
+  describe("filter",function(){
+
+    it("it should give all even numbers",function(){
+      deepEqual(filter(isEven,[1,2,3,4,4,2,5,6]),[2,4,4,2,6]);
+    });
+
+    it("it should give all even numbers",function(){
+      deepEqual(filter(isOdd,[3,4,5,6]),[3,5]);
+    });
+  });
+
+  describe("map",function(){
+
+    it("it should give the squares of the respective numbers",function(){
+      deepEqual(map(getSquare,[1]),[1]);
+      deepEqual(map(getSquare,[1,2,3]),[1,4,9]);
+    });
+  });
+
+  describe("reduce",function(){
+
+    it("it should give the sum of all elements of an array",function(){
+      deepEqual(reduce(sum,[1,2,3,4,5,6]),21);
+    });
+
+    it("it should give undefined as sum",function(){
+      deepEqual(reduce(sum,[]),undefined);
+    });
+
+    it("if the given array is empty than it should give the 0 or initial value of sum",function(){
+      deepEqual(reduce(sum,[],2),2);
+    });
+
+    it("sum",function(){
+      deepEqual(reduce(sum,[1,-1],-2),-2);
+    });
+
+    it("factorial",function(){
+      deepEqual(reduce(factorial,[1]),1);
+    });
+
+    it("factorial",function(){
+      deepEqual(reduce(factorial,[1,2,3,4]),24);
+    });
+
+    it("factorial",function(){
+      deepEqual(reduce(factorial,[1,4,5,0]),0);
+    });
+
+  });
+});
